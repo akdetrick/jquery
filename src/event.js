7f673f575569c0f77b16c48a5707ab453dc66421
Adam Detrick <akdetrick@gmail.com>
2013-01-02 13:01:53 -0500

    Update src/event.js
    stub transitionend

--- src/event.js
+++ src/event.js
@@ -796,7 +796,8 @@ jQuery.fn.extend({
 
 jQuery.each( ("blur focus focusin focusout load resize scroll unload click dblclick " +
 	"mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
-	"change select submit keydown keypress keyup error contextmenu").split(" "), function( i, name ) {
+	"change select submit keydown keypress keyup error contextmenu " + 
+	"transitionend").split(" "), function( i, name ) {
 
 	// Handle event binding
 	jQuery.fn[ name ] = function( data, fn ) {
